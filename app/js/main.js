3c0634be8ac07e23cf1b07ef3f5ea43c39cf1174
Tanya <tanja.synjak.98@ukr.net>
2023-02-07 23:43:57 +0200

    адаптив секції footer

--- app/js/main.js
+++ app/js/main.js
@@ -26,3 +26,17 @@ var swiper = new Swiper('.customers__swiper', {
     },
   },
 });
+
+const acc = document.getElementsByClassName('accordion');
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener('click', function () {
+    this.classList.toggle('active');
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + 'px';
+    }
+  });
+}
